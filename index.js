5d350a2c43eeb24319325ac74a465e4fff618822
kurtsvendsen <55398982+kurtsvendsen@users.noreply.github.com>
2019-10-23 09:42:01 +0200

    Sonjas hjemmeside

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+function denGode() {
+    alert("Jeg forslår at du tager afsted ?");
+}
+
+function denBarske() {
+    alert("Kom nu bare afsted !!");
+}
+
+function sandheden() {
+    alert("Kurt rejs og vær glad");
+}
+
+function engelsk() {
+    alert("Engelsk er nødvendigt. Og jeg mestre det både skriftligt og mundligt");
+}
+
+function tysk() {
+    alert("Tysk er nok mit dårligste sprog.  Mundligt går det fint men mangler lidt på det skriftlige.");
+}
+
+function fransk() {
+    alert("Fransk er et smukt sprog som jeg mestre både skriftligt og mundligt ");
+}
+
+function italiensk() {
+    alert("Italiensk er mit ynglingssprog.  Både skriftligt og særdeles mundligt ");
+}
